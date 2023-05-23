79e5400fd3a1761eb385d77173f3fc4697135e8c
Hagr Saad <hagrsaad16@gmail.com>
2023-05-22 20:42:12 -0400

    Create grid board"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 console.log("Hello World")
+let container = document.querySelector('.container');
+
+function createRow(num){
+  let row = document.createElement('div');
+  row.classList.add("flex-row");
+
+  for(let i = 0; i < num; i++){
+    let grid = document.createElement('div');
+    grid.classList.add("flex-square");
+    row.appendChild(grid);
+  }
+
+  container.appendChild(row);
+}
+
+function createBoard(num){
+  for(let i = 0; i < num; i++){
+    createRow(num);
+  }
+//     restartButton = document.createElement('button');
+//     let parentContainer = document.querySelector('body');
+
+//     restartButton.id = "restart";
+//     restartButton.textContent = "Play Again?";
+//     restartButton.addEventListener('click', () => refreshPage());
+
+//     parentContainer.appendChild(restartButton);
+}
+
+createBoard(16);
